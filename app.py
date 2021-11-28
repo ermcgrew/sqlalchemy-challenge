fd83a0e477c35af5c5a147bf4638cebd85431a62
PlanetM <ermcgrew@users.noreply.github.com>
2021-11-28 17:24:31 -0500

    start/end routes done

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ Station = Base.classes.station
 #Flask setup
 app = Flask(__name__)
 
-#Flask routes
+
+#####Flask routes
+#Root route
 @app.route("/")
 def Home():
     return(
@@ -30,11 +32,12 @@ def Home():
     f"/api/v1.0/precipitation<br/>"
     f"/api/v1.0/stations<br/>"
     f"/api/v1.0/tobs<br/>"
-    f"/api/v1.0/<start><br/>"
-    f"/api/v1.0/<start>/<end><br/>"
+    f"/api/v1.0/YYYY-MM-DD (from given date to end of data)<br/>"
+    f"/api/v1.0/YYYY-MM-DD/YYYY-MM-DD (from given date to given date)<br/>"
     ) 
 
 
+#All precipitation data
 @app.route("/api/v1.0/precipitation")
 def precip():
     #connect to db, query, and close connection
@@ -42,7 +45,7 @@ def precip():
     results = session.query(Measurement.date, Measurement.prcp).group_by(Measurement.date).all()
     session.close()
     
-    #Convert the query results to a dictionary using date as the key and prcp as the value.
+    #Convert the query results to a dictionary
     precip = []
     for date, prcp in results:
         day_dict = {}
@@ -53,6 +56,8 @@ def precip():
     #Return JSON representation
     return jsonify(precip)
 
+
+#List of stations
 @app.route("/api/v1.0/stations")
 def stations():
     #connect to db, query, and close connection
@@ -60,51 +65,58 @@ def stations():
     results = session.query(Station.station).all()
     session.close()
     
+    #convert query result to list
     station_list = list(np.ravel(results))
-
-    #Return a JSON list of stations from the dataset
+    #Return a JSON list
     return jsonify(station_list)
 
+
+#Temperature observations for the most active station for the previous year.
 @app.route("/api/v1.0/tobs")
 def temp():
+    #calculate the dt variable for the last year of the dataset
+    oneyearago = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    #connect to db, query, and close connection
     session = Session(engine)
-    #Query the dates and temperature observations of the most active station for the last year of data.
-    oneyearago = dt.date(2017,8,23) - dt.timedelta(days=365) #max(Measurement.date)
-
-    # busy_station = session.query(Measurement.station, func.count(Measurement.prcp)).\
-    #     group_by(Measurement.station).order_by(func.count(Measurement.prcp).desc()).first()
-
     results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= oneyearago).group_by(Measurement.date).all()
-    
     session.close()
 
+    #convert query result to list
     temp_list = list(np.ravel(results))
-
-    # Return a JSON list of temperature observations (TOBS) for the previous year. 
+     #Return a JSON list
     return jsonify(temp_list)
 
+
+#Temperature MIN, AVG, and MAX for all dates >= to the start date.
 @app.route("/api/v1.0/<start>")
-def startonly():
-    return
-# Return a JSON list of the minimum temperature, 
-# the average temperature, and the max temperature 
-# for a given start or start-end range.
+def startonly(start):   
+    #connect to db, query, and close connection
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).first()
+    session.close()
+    
+    #convert query result to list
+    temp_stats = list(np.ravel(results))
+    #Return a JSON list
+    return jsonify(temp_stats)
 
-#calculate TMIN, TAVG, and TMAX for all dates 
-# greater than and equal to the start date.
 
+#Temperature MIN, AVG, and MAX for dates between the start and end date inclusive.
 @app.route("/api/v1.0/<start>/<end>")
-def startend():
-    return
-# Return a JSON list of the minimum temperature, 
-# the average temperature, and the max temperature 
-# for a given start or start-end range.
+def startend(start, end):
+    #connect to db, query, and close connection
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+    session.close()
 
-#When given the start and the end date, 
-# calculate the TMIN, TAVG, and TMAX 
-# for dates between the start and end date inclusive.
-
+    #convert query result to list
+    temp_stats = list(np.ravel(results))
+    #Return a JSON list
+    return jsonify(temp_stats)
 
 
 if __name__ == "__main__":
